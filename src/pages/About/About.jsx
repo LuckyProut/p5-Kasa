fa15997d07db8f5a8d19bc9fb61e709773cb869d
LuckyProut <luc.garrouste@gmail.com>
2024-10-17 20:52:59 +0200

    all features work, just need to feat media querry

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,6 +3,7 @@ import Banner from '../../components/banner/banner';
 import Collapse from '../../components/collapse/collapse';
 import bannerImg from "../../img/bannerImg2.jpg"
 import Data from '../../data/information.json'
+import './about.scss'
 
 
 const About = () => {
