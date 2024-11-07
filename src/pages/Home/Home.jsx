37986413b1d3a48d0ecb81ed49867ded173b1f17
LuckyProut <luc.garrouste@gmail.com>
2024-11-07 14:39:43 +0100

    fix card gallery

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import Banner from '../../components/banner/banner';
 import bannerImg from "../../img/bannerImg.png"
-import CardGallery from '../../components/cardgallery/cardgallery';
+import Card from '../../components/card/card';
+import Data from '../../data/data.json'
+import '../../style/cardarray.scss'
 import '../../style/home.scss';
 
 const Home = () => {
   return (
-    <><Banner 
+    <>
+      <Banner 
         picture={bannerImg}
-        title={<>
-          Chez vous, <br className="mobile-br" />
-          partout et ailleurs
-      </>
-      }
-        />
-      <CardGallery /></>
+        title={
+          <>
+            Chez vous, <br className="mobile-br" />
+            partout et ailleurs
+          </>
+        }
+      />
+      <section className='location__background'>
+        {Data.map((locationData) => (
+          <Card
+            key={locationData.id}
+            id={locationData.id}
+            title={locationData.title}
+            cover={locationData.cover}
+          />
+        ))}
+      </section>
+    </>
   );
 };
 
